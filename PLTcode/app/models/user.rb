ff4a8ef6de20e78bf8a5b7627227f4722b3235b6
jeonguny <wn28700@gmail.com>
2014-10-08 14:25:07 -0700

    Put a constraint on password length while doing sign up in sign up form

--- PLTcode/app/models/user.rb
+++ PLTcode/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   default_scope -> { order('created_at DESC') }
   validates_uniqueness_of :username
-  validates_confirmation_of :password
+  validates_confirmation_of :password, length: { in: 6..20 }
   validates_presence_of :username, :role, :login_approval, :first_name, :last_name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
